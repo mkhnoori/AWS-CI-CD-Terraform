b8acff858a729889031491e49169af581515b2ee
mkhnoori <mkh_noori@yahoo.com>
2024-11-07 12:43:55 -0500

    modified

--- terraform-manifests/ec2-bastion.tf
+++ terraform-manifests/ec2-bastion.tf
@@ -2,7 +2,7 @@ module "ec2-instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.7.1"
   name = "${var.environment}-BastionHost"
-  ami = data.aws_ami_ids.amzlinux2.id
+  ami = data.aws_ami.amzlinux2.id
   instance_type = var.instance_type
   key_name = var.instance_keypair
   subnet_id = module.vpc.public_subnets[0]
--- terraform-manifests/vpc-module.tf
+++ terraform-manifests/vpc-module.tf
@@ -7,8 +7,8 @@ module "vpc" {
   public_subnets = var.vpc_public_subnets
   private_subnets = var.vpc_private_subnets
   database_subnets = var.vpc_database_subnets
-  create_database_subnet_group = var.vpc_create_db_subnet_group
-  create_database_subnet_route_table = var.vpc_create_db_subnet_route_table
+  create_database_subnet_group = var.vpc_create_database_subnet_group
+  create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
   enable_nat_gateway = var.vpc_enable_nat_gateway
   single_nat_gateway = var.vpc_single_nat_gateway
 
